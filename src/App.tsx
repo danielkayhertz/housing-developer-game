7991d8252726fd50e85af99c9938a8693ef201eb
BPI <bpi@bpichicago.org>
2026-06-02 23:46:34 -0500

    scaffold: Vite + React 19 + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl">Chicago Affordable Housing Developer</h1>
+    </div>
+  );
+}
